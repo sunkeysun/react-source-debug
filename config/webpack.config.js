fdaa1ecb6e76b05c1a421d16a7b5c4ff07ecbccc
sunkeysun <sunkeysun@tencent.com>
2021-07-21 20:18:30 +0800

    update

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -328,10 +328,10 @@ module.exports = function (webpackEnv) {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
-        'react': path.resolve(__dirname, '../src/react/packages/react'),
-        'react-dom': path.resolve(__dirname, '../src/react/packages/react-dom'),
-        'shared': path.resolve(__dirname, '../src/react/packages/shared'),
-        'react-reconciler': path.resolve(__dirname, '../src/react/packages/react-reconciler'),
+        'react': path.resolve(__dirname, '../src/react17/packages/react'),
+        'react-dom': path.resolve(__dirname, '../src/react17/packages/react-dom'),
+        'shared': path.resolve(__dirname, '../src/react17/packages/shared'),
+        'react-reconciler': path.resolve(__dirname, '../src/react17/packages/react-reconciler'),
         // Allows for better profiling with ReactDevTools
         ...(isEnvProductionProfile && {
           'react-dom$': 'react-dom/profiling',
